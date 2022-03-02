d26529584d41b21da319a675d3baea6ae8a63d9b
CSI-Juan-Jimenez <26-028@sanignacio.pr>
2022-03-02 15:01:59 -0400

    Create 1dig7segName.ino

---
+++ 1dig7segName.ino
@@ -0,0 +1,502 @@
+const int pinA = 2;
+const int pinB = 3;
+const int pinC = 4;
+const int pinD = 5;
+const int pinE = 6;
+const int pinF = 7;
+const int pinG = 8;
+const int pinH = 9;
+const int decimal = 10;
+
+void setup() {
+// put your setup code here, to run once:
+pinMode(pinA, OUTPUT);     
+pinMode(pinB, OUTPUT);     
+pinMode(pinC, OUTPUT);     
+pinMode(pinD, OUTPUT);     
+pinMode(pinE, OUTPUT);     
+pinMode(pinF, OUTPUT);     
+pinMode(pinG, OUTPUT);
+pinMode(pinH, OUTPUT);
+pinMode(decimal, OUTPUT);
+
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, HIGH);
+digitalWrite(pinH, HIGH);
+delay(100);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+digitalWrite(pinH, LOW);
+delay(100);
+digitalWrite(pinA, HIGH);
+digitalWrite(pinB, HIGH);
+digitalWrite(pinC, HIGH);
+digitalWrite(pinD, HIGH);
+digitalWrite(pinE, HIGH);
+digitalWrite(pinF, HIGH);
+digitalWrite(pinG, HIGH);
+digitalWrite(pinH, HIGH);
+delay(100);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+digitalWrite(pinH, LOW);
+digitalWrite(decimal, LOW);
+}
+
+void loop() {
+// put your main code here, to run repeatedly:
+
+delay(500);
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(1000);
+
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// J
+
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+digitalWrite(pinH, LOW);
+delay(500);
+//U
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(50);
+digitalWrite(pinG, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+digitalWrite(pinH, LOW);
+delay(500);
+// A
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// N
+
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinG, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(500);
+
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// P
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(50);
+digitalWrite(pinG, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// A
+
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(50);
+digitalWrite(pinG, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// B
+
+digitalWrite(pinF, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// L
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// O
+
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(500);
+
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// J
+
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(500);
+
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// I
+
+digitalWrite(pinF, HIGH);
+delay(50);
+digitalWrite(pinG, HIGH);
+delay(50);
+digitalWrite(pinB, HIGH);
+delay(500);
+
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// M
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(50);
+digitalWrite(pinG, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(500);
+
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// E
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(1000);
+// N
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(50);
+digitalWrite(pinG, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(500);
+
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// E
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinG, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(500);
+
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// Z
+
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(50);
+digitalWrite(pinG, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+//B
+
+digitalWrite(pinF, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// L
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(50);
+digitalWrite(pinG, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+digitalWrite(pinH, LOW);
+delay(500);
+// A
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// N
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// C
+
+digitalWrite(pinA, HIGH);
+delay(50);
+digitalWrite(pinB, HIGH);
+delay(50);
+digitalWrite(pinC, HIGH);
+delay(50);
+digitalWrite(pinD, HIGH);
+delay(50);
+digitalWrite(pinE, HIGH);
+delay(50);
+digitalWrite(pinF, HIGH);
+delay(500);
+digitalWrite(pinA, LOW);
+digitalWrite(pinB, LOW);
+digitalWrite(pinC, LOW);
+digitalWrite(pinD, LOW);
+digitalWrite(pinE, LOW);
+digitalWrite(pinF, LOW);
+digitalWrite(pinG, LOW);
+delay(500);
+// O
+}
